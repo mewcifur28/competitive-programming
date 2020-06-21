980f4f1d667e3014cd5d060ab3b41650ccaf6358
Ishita Gambhir <ishita.gambhir28@gmail.com>
2020-06-21 21:10:49 +0530

    added programs

---
+++ mathematics/favdice-spoj.cpp
@@ -0,0 +1,18 @@
+// https://www.spoj.com/problems/FAVDICE/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main() {
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        float ans = 0;
+        for (ll i = 1; i <= n; i++) {
+            ans += (1/(i * 1.0));
+        }
+        ans *= n;
+        cout << fixed << setprecision(2) << ans << "\n";
+    }
+}
