93610efe0d51ab97935ee96d9be46485703f33a6
Ishita Gambhir <ishita.gambhir28@gmail.com>
2020-06-25 04:51:12 +0530

    added programs

---
+++ mathematics/dice-expectations-I.cpp
@@ -0,0 +1,9 @@
+// What is the expected number of dice throws required to get a "four"?
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	float p = 1.0/6;
+	cout << ceil(1/p);
+	return 0;
+}
